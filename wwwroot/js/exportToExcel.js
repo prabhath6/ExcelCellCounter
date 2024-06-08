d42c09acb429143057c2344b1ad3e97f47dc89a5
prabhath6 <prabhathkiran20@gmail.com>
2024-06-08 14:35:10 -0500

    Add range column to the table

--- wwwroot/js/exportToExcel.js
+++ wwwroot/js/exportToExcel.js
@@ -1,7 +1,7 @@
 function exportToExcel(tableData) {
     // Create a new workbook
     var wb = XLSX.utils.book_new();
-    
+
     // Convert the TableData object to a 2D array
     var ws_data = [
         ["Id", "Text", "Rows", "Columns", "Total Cells"],
@@ -13,13 +13,13 @@ function exportToExcel(tableData) {
             ws_data.push([i + 1, tableData[i].range, tableData[i].rows, tableData[i].columns, tableData[i].totalCells]);
         }
     }
-    
+
     // Create a worksheet from the data array
     var ws = XLSX.utils.aoa_to_sheet(ws_data);
-    
+
     // Append the worksheet to the workbook
     XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-    
+
     // Generate the Excel file and trigger the download
     XLSX.writeFile(wb, "export.xlsx");
 }
