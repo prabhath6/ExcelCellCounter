37a941a69e61d03e447552b388922123bcfbbba7
prabhath6 <prabhathkiran20@gmail.com>
2024-06-10 23:37:16 -0500

    Update site as per lighthouse

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 using ExcelCellCounter.Components;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddResponseCompression(options =>
+{
+	options.Providers.Add<BrotliCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+    options.EnableForHttps = true;
+});
+
 
 var app = builder.Build();
 
@@ -15,6 +24,23 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Use(async (context, next) =>
+{
+	context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' 'nonce-somestring' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; base-uri 'self';");
+	await next();
+});
+
+
+app.UseResponseCompression();
+app.UseStaticFiles(new StaticFileOptions
+{
+	OnPrepareResponse = ctx =>
+	{
+		// Cache static files for 30 days
+		ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=31536000");
+	}
+});
 
 app.UseHttpsRedirection();
 
